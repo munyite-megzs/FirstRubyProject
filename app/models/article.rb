36fd4f80dced813a9ef419cca8c75a01e093a3d1
munyite-megzs <margaret.sitati1@gmail.com>
2015-04-17 08:53:41 +0300

    added a few gems

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,26 @@
 class Article < ActiveRecord::Base
 	has_many :comments
+	has_many :taggings
+
+	has_many :tags, through: :taggings
+
+#method to display the tags
+#converting all our objects to an array of tag names
+#Joining the array of tag names together
+	def tag_list
+		self.tags.collect do |tag|
+			tag.name
+		end.join(", ")
+	end
+#use to represent a special tags e.g "Ruby, programing"
+	def tag_list=(tags_string)
+  tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
+  new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
+  self.tags = new_or_found_tags
 end
+
+
+end
+
+
+
